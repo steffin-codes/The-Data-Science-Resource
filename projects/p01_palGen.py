495162f9afca9a08e1d0ca08e309642e7ed0dbeb
steffincodes <steffin.codes@gmail.com>
2021-07-14 17:33:22 +0530

    🔥 @st.cache? and ♿️ (#3)

--- projects/p01_palGen.py
+++ projects/p01_palGen.py
@@ -23,26 +23,17 @@ def app():
         modified_img = cv2.resize(raw_img, raw_img.shape[:2], interpolation = cv2.INTER_AREA) #optional
         modified_img = modified_img.reshape(modified_img.shape[0]*modified_img.shape[1], 3) #required
         return modified_img
-    @st.cache
-    def cluster():
-        modified_image = prep_image(orig_image)
-        clf,color_labels,center_colors,counts,ordered_colors,hex_colors=None,None,None,None,None,None
-        clf = KMeans(n_clusters=n_clusters)
-        color_labels = clf.fit_predict(X=modified_image,y=None)
-        center_colors = clf.cluster_centers_
-        counts = Counter(color_labels)
-        ordered_colors = [center_colors[i] for i in counts.keys()]
-        hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
-        fig, ax = plt.subplots()
-        ax.pie(counts.values(), labels = hex_colors, colors = hex_colors)
-        return fig
-    
+ 
     # project inputs
-    st.title("Palatte generator using K-Means")
-    uploaded_file = st.sidebar.file_uploader("Upload Files",type=['png','jpeg'])
-    n_clusters = st.sidebar.slider('How many colors?', 1, 10, 4)
+    st.title("Palette generator using K-Means")
+    st.write("Presenting a simple implementation of the K-means ML algorithm. This web app requires an image, which will be fed to the K-means algorithm, which will detect the colors and calculates the wieghts of each color. This is then graphed into a pie chart that looks like a Palette!")
+    st.sidebar.subheader("Project Inputs:")
+    uploaded_file = st.sidebar.file_uploader("Upload Image",type=['png','jpeg'],help="Bigger the image accurate the clustering would be!")
+    n_clusters = st.sidebar.slider('Value of K?', 1, 10, 4,help="No of colors!")
+    generate = st.sidebar.button("Generate",help="Clicke me to generate the chart!")
     
     if uploaded_file is not None:
+        # orig_image = None
         # Convert the file to an opencv image.
         file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
         opencv_image = cv2.imdecode(file_bytes, 1)
@@ -52,9 +43,19 @@ def app():
             st.subheader("Image")
             st.image(orig_image)
         with col2:
-            st.subheader("Palette Generator")
-            fig = cluster()
-            st.pyplot(fig)
+            if (generate):
+                st.subheader("The Palette using k={}".format(n_clusters))
+                modified_image = prep_image(orig_image)
+                clf,color_labels,center_colors,counts,ordered_colors,hex_colors=None,None,None,None,None,None
+                clf = KMeans(n_clusters=n_clusters)
+                color_labels = clf.fit_predict(X=modified_image,y=None)
+                center_colors = clf.cluster_centers_
+                counts = Counter(color_labels)
+                ordered_colors = [center_colors[i] for i in counts.keys()]
+                hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
+                fig, ax = plt.subplots()
+                ax.pie(counts.values(), labels = hex_colors, colors = hex_colors)
+                st.pyplot(fig)
     else:
         st.warning("👈🏼 Head over to the sidebar and upload an image!")    
     pass
