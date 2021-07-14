e28a40ce6e38d5af0755600fcff14a8cf66476a7
steffincodes <steffin.codes@gmail.com>
2021-07-14 16:34:06 +0530

    🦢 Remove button in search menu

--- apps/search.py
+++ apps/search.py
@@ -2,13 +2,9 @@ import streamlit as st
 # TODO: give a search functionality to the posts...
 def app():
     st.title('Look through my content maybe?')
-    icon_col,text_col,button_col = st.beta_columns([1,9,1])
+    icon_col,text_col = st.beta_columns([1,9])
     with icon_col:
         st.title("🔍")
     with text_col:
         search_word = st.text_input("Looking for something specific?", "")
-    with button_col:
-        if(st.button("search")):
-            st.write('You are trying to search for: ',search_word)
-    st.write('list of all posts sorted by date.')
-    st.write('With auto search functionality?')
+    
