8c087d21933a7b1eb3940160db17f4710b19ba39
steffincodes <steffin.codes@gmail.com>
2021-07-17 18:24:49 +0530

    🗃️ Listing Projects

--- Helper.py
+++ Helper.py
@@ -1,8 +1,17 @@
 import streamlit as st
 import urllib
+import pandas as pd
 class Functions():
     @st.cache(show_spinner=False)
     def get_file_content_as_string(path):
         url = 'https://raw.githubusercontent.com/steffincodes/data-scribbles/main/' + path
         response = urllib.request.urlopen(url)
         return response.read().decode("utf-8")
+    
+    @st.cache(show_spinner=False)
+    def load_csv_as_dataframe(path):
+        DATA_URL = 'https://raw.githubusercontent.com/steffincodes/data-scribbles/main/' + path
+        data = pd.read_csv(DATA_URL,parse_dates=['Date'])
+        lowercase = lambda x: str(x).lower()
+        data.rename(lowercase, axis='columns', inplace=True)
+        return data
--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,30 @@
 import streamlit as st
+import pandas as pd
+from Helper import Functions
 # TODO: Add credits and instructions for navigation
 def app():
-    st.title('Hello and Welcome to my blog 👋🏼')
-    st.info('Please be informed that besides the effort put in consolidating the application, any data/code/writeup you find here is not my own but rather a brainchild of my google searching capabilities. Care is taken to source to the original authors. But when more than 5 sources are refered to, the top 5 are cited.')
-    st.success('👈🏼 Headover to the sidebar to find my latest projects!')
+    st.title('Hello and Welcome to Steffin\'s Data Playground 👋🏼')
+    st.warning('Please be informed that besides the effort put in consolidating the application, any data/code/writeup you find here is not my own but rather a brainchild of my google searching capabilities. Care is taken to source to the original authors. But when more than 5 sources are refered to, the most relevant are cited.')
+    st.success('👈🏼 Headover to the sidebar to find live demo of the below projects!')
+    list_of_projects = Functions.load_csv_as_dataframe("projects.csv") 
+    df = pd.DataFrame(list_of_projects)
+    df.sort_values(by=['date'],inplace=True,ascending=False)
+    for index, project in df.iterrows():
+        st.markdown('''
+        ***
+        >
+        > `{}`
+        > ## **{}**
+        > > {}
+        > 
+        > *View the accompaning [Google Colab Notebook]({})*
+        >
+        > **Algorithm used:** `{}`
+        '''.format(
+            project["date"].strftime("%c").replace(" 00:00:00",","),
+            project["name"],
+            project["tldr"],
+            project["notebook"],
+            project["algorithm"]
+
+        ))
