bf7838c0a3f3a9fd55d8aad582400b66b190cf31
Steffin <64345932+steffincodes@users.noreply.github.com>
2021-08-06 19:15:17 +0530

    (#5) Linked them both

--- pages/ChooseStatisticModel.py
+++ pages/ChooseStatisticModel.py
@@ -1,13 +1,13 @@
 import streamlit as st
-
-st.header("FIVE QUESTIONS FOR SELECTING A STATISTICAL METHOD")
-
 # preping schema
+from Helper import Functions
+# TODO: Yeah oops I forgot!
+def app():
+  st.header("FIVE QUESTIONS FOR SELECTING A STATISTICAL METHOD")
+  numberOfVariables = st.radio(
+    "How many variables do you have?",
+    ('One','More Than One','Too many')
+  )
 
-numberOfVariables = st.radio(
-  "How many variables do you have?",
-  ('One','More Than One','Too many')
-)
-
-if numberOfVariables == 'Too many':
-  st.success("Conduct a cluster analysis to select representative variables")
+  if numberOfVariables == 'Too many':
+    st.success("Conduct a cluster analysis to select representative variables")
