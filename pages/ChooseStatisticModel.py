77028be1278bb2e99cd8ffb1682edf76f6e991c8
steffincodes <steffin.codes@gmail.com>
2021-08-07 20:27:26 +0530

    (#5)  not happy -.-

--- pages/ChooseStatisticModel.py
+++ pages/ChooseStatisticModel.py
@@ -1,13 +1,128 @@
 import streamlit as st
-# preping schema
-from Helper import Functions
-# TODO: Yeah oops I forgot!
 def app():
-  st.header("FIVE QUESTIONS FOR SELECTING A STATISTICAL METHOD")
-  numberOfVariables = st.radio(
-    "How many variables do you have?",
-    ('One','More Than One','Too many')
-  )
+    st.title('Five Questions for selecting a Statistical Method')
+    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+    
+    variableCount = st.radio(
+      label="How many variables do you have?",
+      options=("None","One","More than One","Too Many")
+    )
+    if variableCount == 'None':
+      st.markdown(
+        '''
+        variableCount : {}
+        '''
+      .format(variableCount))
+    if variableCount == 'One':
+      st.markdown(
+        '''
+        variableCount : {}
+        '''
+      .format(variableCount))
+      statisticalObjective = st.radio(
+        label="What is your statistical objective?",
+        options=("None","Describe","Classify","Compare","Predict","Explain")
+      )
+      if statisticalObjective == 'None':
+        st.markdown(
+          '''
+          variableCount : {}
+          
+          statisticalObjective : {}
+          '''
+        .format(variableCount,statisticalObjective))
+      if statisticalObjective == "Describe":
+        st.markdown(
+          '''
+          variableCount : {}
+          
+          statisticalObjective : {}
 
-  if numberOfVariables == 'Too many':
-    st.success("Conduct a cluster analysis to select representative variables")
+          ## Descriptive Statistics:
+          ### Discrete Scales
+              
+              Counts, proportions
+
+          ### Continuous Scales
+              
+              Means, standard deviations, medians, ranges, quartiles
+
+          ## Distribution Fitting Statistics:
+              
+              Skewness, kurtosis coefficient of variation
+
+          ## Graphics:
+              
+              Histograms, box plots, probability plots
+
+          ## Tests:
+              
+              Kolmogorov-Smirnov, Anderson-Darling, Shapiro-Wilk Lillifors
+
+
+
+          '''
+        .format(variableCount,statisticalObjective))
+      if statisticalObjective == "Classify":
+        st.markdown(
+          '''
+          variableCount : {}
+          
+          statisticalObjective : {}
+          - Filtering
+          - Sorting
+          - Cluster analysis
+          '''
+        .format(variableCount,statisticalObjective))
+      if statisticalObjective == "Compare":
+        st.markdown(
+          '''
+          variableCount : {}
+          
+          statisticalObjective : {}
+          ## Discrete Scale
+              One-Population Test
+              Non-parametric statistics
+          ## Continuous Scales
+              t-tests
+              control charts
+
+          '''
+        .format(variableCount,statisticalObjective))
+      if statisticalObjective == "Predict":
+        st.markdown(
+          '''
+          variableCount : {}
+          
+          statisticalObjective : {}
+          '''
+        .format(variableCount,statisticalObjective))
+        st.warning("Not possible")
+      if statisticalObjective == "Explain":
+        st.markdown(
+          '''
+          variableCount : {}
+          
+          statisticalObjective : {}
+          '''
+        .format(variableCount,statisticalObjective))
+        st.warning("Not possible")
+    if variableCount == 'More than One':
+      st.markdown(
+        '''
+        variableCount : {}
+        '''
+      .format(variableCount))
+    if variableCount == 'Too Many':
+      st.markdown(
+        '''
+        variableCount : {}
+        
+        Conduct a cluster analysis to select representative variables
+        
+        Conduct a principal components analysis, factor analysis, correspondence analysis, or multidimensional scaling to reduce the number of variables needed to represent most of the variability
+
+        '''
+      .format(variableCount))
+
+    
