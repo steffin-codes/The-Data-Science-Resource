8f5901d6f340aada4161120926c652a9627b19ad
Steffin <64345932+steffincodes@users.noreply.github.com>
2023-03-19 12:16:22 +0000

    🏗️ Listed Books - Need more Data :)

--- pages/98_📚_Reads.py
+++ pages/98_📚_Reads.py
@@ -4,34 +4,43 @@ from styles import *
 st.set_page_config(
     page_title="Reads | Steffin",
     page_icon="📚",
-    layout="wide"
+    layout="wide",
+    initial_sidebar_state="collapsed"
 )
 
-
-display_line(st)
-display_centered_text(st,"h1","Books I have Read")
-display_line(st)
-display_centered_text(st,"p","There's a lot of knowledge and content everywhere, but books remain the most condensed form of wisdom that I rely on. I thought I would maintain an eclectic set of books that I have been educated by. This list is a constant work in progress.")
-display_line(st,"5em")
-book_list = [
-    # {
+BOOKS = [
+    {
+        "book_name":"Worm",
+        "author":"Wildbow (John C. McCrae)",
+        "book_img":"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1519662877l/18713259.jpg",
+        "book_one_liner":"Trauma induced unconventional superpowers in teenage students."
+    # },{
     #     "book_name":"",
     #     "author":"",
     #     "book_img":"",
     #     "book_one_liner":""
-    # }
+    }
 ]
-n_columns = 3
-book_columns = st.columns(n_columns)
-for i, book_data in enumerate(book_list):
-    with book_columns[i%n_columns]:
-        _col=st.columns([1,5,1])
-        display_book_image(
-            _col[1],
-            img_src=book_data["book_img"],
-            caption=book_data["book_one_liner"])
-    # display_centered_text(
-    #     book_columns[i%n_columns],
-    #     "p",
-    #     f'{book_data["book_name"]} | {book_data["author"]}'
-    #     )
+def Header():
+    display_line(st)
+    display_centered_text(st,"h1","My Reading List")
+    display_line(st)
+    display_centered_text(st,"p","Was a voracious reader in my teens. Looking to use this page to hold myself Accountable and get back into the habit.")
+    pass
+def ListBooks():
+    n_columns = 3
+    book_columns = st.columns(n_columns)
+    for i, book_data in enumerate(BOOKS):
+        with book_columns[i%n_columns]:
+            _col=st.columns([1,5,1])
+            display_book_image(
+                _col[1],
+                img_src=book_data["book_img"],
+                caption=book_data["book_one_liner"])
+    pass
+def BooksApp():
+    Header()
+    display_line(st,"5em")
+    ListBooks()
+    pass
+BooksApp()
